e8b23c7c7d08736ece15e75b088d8e6f89d4a5e6
maksim <maksim.maestto@gmail.com>
2023-12-08 00:03:11 +0300

    JSX to TSX and refactoring

--- src/components/question.tsx
+++ src/components/question.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
 import Slider from '@mui/material/Slider';
 
-const sliderMarks = [
+interface SliderMark {
+    value: number
+    label: string
+}
+
+const sliderMarks: SliderMark[] = [
     { value: 0, label: '0', },
     { value: 1, label: '1', },
     { value: 2, label: '2', },
@@ -15,30 +20,38 @@ const sliderMarks = [
     { value: 10, label: '10', },
 ]
 
-export default function Question({ questionID, questionText, updateTotalScore, allScoresArr }) {
-    const [questionScore, setQuestionScore] = React.useState(0)
+interface QuestionProps {
+    questionID: number
+    questionText: string
+    updateTotalScore: (questionID: number, newValue: number) => void
+    allScoresArr: number[]
+}
 
-    const calculateTotalScoreWithNewValue = (newValue) => {
+const Question: React.FC<QuestionProps> = ({ questionID, questionText, updateTotalScore, allScoresArr }) => {
+    const [questionScore, setQuestionScore] = React.useState<number>(0)
+
+    const calculateTotalScoreWithNewValue = (newValue: number) => {
         const updatedScores = [...allScoresArr]
         updatedScores[questionID] = newValue
 
         return updatedScores.reduce((sum, newValue) => sum + newValue, 0)
     }
 
-    const onSliderChange = (_, newValue) => {
-        const totalScoreWithNewValue = calculateTotalScoreWithNewValue(newValue)
+    const onSliderChange = (_: Event, newValue: number | number[]): void => {
+        let value = Array.isArray(newValue) ? newValue[0] : newValue
+        const totalScoreWithNewValue = calculateTotalScoreWithNewValue(value)
 
         if (totalScoreWithNewValue > 10) {
-            for (let i = newValue; i >= 0; i--) {
+            for (let i = value; i >= 0; i--) {
                 if (calculateTotalScoreWithNewValue(i) <= 10) {
-                    newValue = i;
-                    break;
+                    value = i
+                    break
                 }
             }
         }
 
-        setQuestionScore(newValue)
-        updateTotalScore(questionID, newValue)
+        setQuestionScore(value)
+        updateTotalScore(questionID, value)
     }
 
     return (
@@ -59,3 +72,5 @@ export default function Question({ questionID, questionText, updateTotalScore, a
         </div>
     )
 }
+
+export default Question
--- src/components/questionsSet.tsx
+++ src/components/questionsSet.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import Question from "./question";
 
-const questionsData = [
+const questionsData: string[] = [
     'Вопрос 1',
     'Вопрос 2',
     'Вопрос 3'
 ]
 
-const QuestionsSet = () => {
-    const [allScoresArr, setAllScoresArr] = React.useState([])
-    const [totalScore, setTotalScore] = React.useState(0)
+const QuestionsSet: React.FC = () => {
+    const [allScoresArr, setAllScoresArr] = React.useState<number[]>([])
+    const [totalScore, setTotalScore] = React.useState<number>(0)
 
-    const updateTotalScore = (questionID, questionScore) => {
-        const updatedScores = [...allScoresArr]
+    const updateTotalScore = (questionID: number, questionScore: number): void => {
+        const updatedScores: number[] = [...allScoresArr]
         updatedScores[questionID] = questionScore
         setAllScoresArr(updatedScores)
 
