767b16010b011342f2ddd5694c9c32733d65cbef
maksim <maksim.maestto@gmail.com>
2023-12-09 03:44:40 +0300

    Added react-router-dom and welcome page

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import QuestionsSet from "./components/questionsSet/questionsSet";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+
+import WelcomePage from "./welcomePage"
+import QuestionsSet from "./components/questionsSet/questionsSet"
+
+import './index.css'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
-      <QuestionsSet/>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<WelcomePage/>}/>
+                <Route path="/questions" element={<QuestionsSet/>}/>
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 )
